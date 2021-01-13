8fd5525e760012777ed5ae2d6e826d21f34f0f4e
johnFarkasolo <farkasolo@mail.ru>
2021-01-14 00:12:23 +0100

    Complited main-content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PokemonMainComponent} from './pokemon-main/pokemon-main.component';
+import {PokemonDetailComponent} from './pokemon-detail/pokemon-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: PokemonMainComponent},
+  {path: 'pokeDetail/:id', component: PokemonDetailComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home' },
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getPokemons() {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=20`);
+  }
+
+  getMoreData(name: string) {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  }
 }
